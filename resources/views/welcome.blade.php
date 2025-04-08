571afd78597dcc6f96d4bb89991591b2fdbbedd7
Marzieh Khayati <marziekhayati@gmail.com>
2025-04-08 20:46:41 +0330

    firs commit(add project)

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>  
+<html lang="fa">  
+<head>  
+    <meta charset="UTF-8">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <title>صفحه اصلی</title>  
+    <style>  
+        body {  
+            background-color: rgb(116, 211, 174);  
+            font-family: Arial, sans-serif;  
+            margin: 0;  
+            display: flex;  
+            flex-direction: column;  
+            align-items: center;  
+            min-height: 100vh;  
+        }  
+
+        .container {  
+            background-color: rgb(246, 231, 203);  
+            border-radius: 20px;  
+            padding: 20px;  
+            text-align: center;  
+            width: 80%; /* طول کادر 80 درصد طول صفحه */  
+            margin-top: 10vh; /* قرارگیری در نیمه بالای صفحه */  
+        }  
+
+        .title {  
+            color: rgb(103, 141, 88);  
+            font-size: 2em;  
+            margin-bottom: 10px;  
+        }  
+
+        .subtitle {  
+            color: rgb(221, 151, 135);  
+            font-size: 1.2em;  
+        }  
+    </style>  
+</head>  
+<body>  
+    <div class="container">  
+        <h1 class="title">گلخانه</h1>  
+        <p class="subtitle">ارائه خدمات گیاه پزشکی</p>  
+    </div>  
+</body>  
+</html>  
